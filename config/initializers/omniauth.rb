774557143f96493d8e64d327554da43d6db9bb13
John K. Towery <kbaries@gmail.com>
2016-08-23 17:06:39 -0600

    Fix quote for keys  in omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,8 +2,8 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2,
-	ENV[GOOGLE_CLIENT_ID],
-	ENV[GOOGLE_CLIENT_SECRET],
+	ENV['GOOGLE_CLIENT_ID'],
+	ENV['GOOGLE_CLIENT_SECRET'],
 	{
 		access_type: 'offline',
     		scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar',
