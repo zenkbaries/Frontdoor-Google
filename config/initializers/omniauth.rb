a8f048e5f651cc2932c970552a97fab0f97bb285
John K. Towery <kbaries@gmail.com>
2016-08-23 17:03:49 -0600

    Create config/initializers/omniauth.rb

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,16 @@
+OmniAuth.config.logger = Rails.logger
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2,
+	ENV[GOOGLE_CLIENT_ID],
+	ENV[GOOGLE_CLIENT_SECRET],
+	{
+		access_type: 'offline',
+    		scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar',
+		client_options: {
+			ssl: {
+				ca_file: Rails.root.join("cacert.pem").to_s
+			}
+		}
+	}
+end
